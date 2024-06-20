aafed84a4d8d22b9089c58e170734d5b7f6fd689
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2024-06-20 09:30:11 +0900

    feat: 코타키나발루 1편 상세 내용 추가

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,27 +6,53 @@
 interface Contents {
   title: string;
   summary?: string;
+  date?: string;
   contents: Array<{ image?: string; texts: string }>;
 }
 
+const ContentsMapper: Record<string, string> = {
+  "1": "http://localhost:3000/posts/detail/kota-kinabalu.json",
+} as const;
+
 const getData = async (id: string) => {
-  const res = await fetch(
-    "http://localhost:3000/posts/detail/kota-kinabalu.json",
-    {
+  try {
+    const res = await fetch(ContentsMapper[id], {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-    }
-  );
+    });
 
-  return res.json();
+    return res.json();
+  } catch (error) {}
 };
 
-const PostDetailPage = async ({ params }: { params: { slug: string } }) => {
-  const data: Contents = await getData(params.slug);
+const PostDetailPage = async ({ params }: { params: { id: string } }) => {
+  const data: Contents = await getData(params.id);
 
-  return <div>{data.title}</div>;
+  if (!data) {
+    return (
+      <div className="w-full font-semibold text-[20px] flex mt-[20px] justify-center">
+        Not Found 🙃
+      </div>
+    );
+  }
+
+  console.log(data);
+
+  return (
+    <div className="w-full flex justify-center p-[24px] gap-[8px] flex-col items-center">
+      <h1 className="text-[28px] font-semibold">{data.title}</h1>
+      {data.summary && <p className="mt-[16px]">{data.summary}</p>}
+      <div className="contenst max-w-[720px] mt-[32px] grid grid-cols-1 gap-[12px] justify-center px-[8px] bg-gray-50 rounded-[8px]">
+        {data.contents.map(({ image, texts }) => (
+          <p key={texts} className="p-[8px]">
+            {texts}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostDetailPage;
