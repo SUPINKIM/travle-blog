394ecf0fe58abd01ffeace8fccca30576a229ca8
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2024-10-09 14:48:01 +0900

    feat: 포스팅 몽고에 저장 및 불러오기 추가

--- src/app/(post)/posts/[id]/page.tsx
+++ src/app/(post)/posts/[id]/page.tsx
@@ -3,32 +3,35 @@
  * @link [https://nextjs.org/docs/app/building-your-application/optimizing/metadata]
  */
 
+import { Countries } from "@/components/home/contents/types";
+import { client } from "@/contant/mongo";
+
 interface Contents {
   title: string;
-  summary?: string;
-  date?: string;
-  contents: Array<{ image?: string; texts: string }>;
+  id: number;
+  contents: string;
+  country: Countries;
 }
-
-const ContentsMapper: Record<string, string> = {
-  "1": "http://localhost:3000/posts/detail/kota-kinabalu.json",
-} as const;
 
 const getData = async (id: string) => {
   try {
-    const res = await fetch(ContentsMapper[id], {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    await client.connect();
 
-    return res.json();
-  } catch (error) {}
+    const res = await client
+      .db("travel")
+      .collection("posts")
+      .findOne<Contents>({ id: parseInt(id) });
+
+    await client.close();
+
+    return res;
+  } catch (error) {
+    return undefined;
+  }
 };
 
 const PostDetailPage = async ({ params }: { params: { id: string } }) => {
-  const data: Contents = await getData(params.id);
+  const data = await getData(params.id);
 
   if (!data) {
     return (
@@ -41,13 +44,14 @@ const PostDetailPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="w-full flex justify-center p-[24px] gap-[8px] flex-col items-center">
       <h1 className="text-[28px] font-semibold">{data.title}</h1>
-      {data.summary && <p className="mt-[16px]">{data.summary}</p>}
-      <div className="content max-w-[800px] mt-[32px] grid grid-cols-1 gap-[12px] justify-center px-[8px] bg-gray-50 rounded-[8px]">
-        {data.contents.map(({ image, texts }) => (
+      {/* {data.summary && <p className="mt-[16px]">{data.summary}</p>} */}
+      <div className="text-[14px] content max-w-[800px] mt-[32px] grid grid-cols-1 gap-[12px] justify-center p-[24px] bg-gray-50 rounded-[8px]">
+        {/* {data.contents.map(({ image, texts }) => (
           <p key={texts} className="p-[16px]">
             {texts}
           </p>
-        ))}
+        ))} */}
+        <div dangerouslySetInnerHTML={{ __html: data.contents }} />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,14 +28,13 @@ export default async function Home() {
   return (
     <div className="w-full grid grid-cols-1 px-[24px] py-[32px]">
       <Header />
-      <div className="mt-[16px] flex items-center gap-[12px] flex-wrap">
+      <div className="mt-[12px] flex items-center gap-[12px] flex-wrap">
         <LinkButton
           link="https://cut-decade-d21.notion.site/b478021d66f6462182a9a89c5855140c?pvs=4"
           label="운영자는 누구인가? 🤔"
         />
-        <LinkButton link="" label="최신 포스팅 보러 가기" />
       </div>
-      <div className="mt-[28px] grid grid-cols-1 gap-y-[8px] w-full">
+      <div className="mt-[20px] grid grid-cols-1 gap-y-[8px] w-full">
         <ImageCarousel images={newImages} />
         <TripPosts />
       </div>
